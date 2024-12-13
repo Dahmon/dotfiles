34e92fc0d5428cbe7886472d005554b36536d55b
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2024-12-13 20:33:06 +1100

    chore: Add clear screen keybind to wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,4 +5,12 @@ local config = wezterm.config_builder()
 config.color_scheme = "Bamboo"
 config.enable_scroll_bar = true
 
+config.keys = {
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
+}
+
 return config
