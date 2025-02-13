54c626f54627af6971aadc95ba344f7a0e3f5c90
Dahmon Bicheno <dahmon.bicheno@servictoria.com.au>
2025-02-13 16:31:07 +1100

    Update setup script

--- .setup.sh
+++ .setup.sh
@@ -3,9 +3,13 @@
 set -ex
 
 if ! command -v brew &>/dev/null; then
-	echo 'brew is not installed. Installing.'
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+  echo 'brew is not installed. Installing.'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Update to templated file
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.bashrc
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>$HOME/.bashrc
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 brew install chezmoi
