c0add5eb06945f56f1106828b963e41084cffcff
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-02-14 00:12:46 +1100

    fix: Correct plugin config

--- private_dot_config/nvim/lua/plugins/neo-tree.lua
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,15 +1,13 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      opts.filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignores = false,
-          hide_hidden = false, -- windows
-        },
-      }
-    end,
-  },
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function(_, opts)
+    opts.filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignores = false,
+        hide_hidden = false, -- windows
+      },
+    }
+  end,
 }
---
+++ private_dot_config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,7 @@
+return {
+  "mbbill/undotree",
+  opts = {},
+  keys = {
+    { "<leader>U", vim.cmd.UndotreeToggle, mode = { "n" }, desc = "Undotree UI" },
+  },
+}
