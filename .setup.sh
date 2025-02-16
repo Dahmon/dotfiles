a93111c0e29085f40d09347a631f80af4b6f0216
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-02-16 03:59:22 +0000

    fix: Correct if statements

--- .setup.sh
+++ .setup.sh
@@ -4,20 +4,20 @@ set -e
 
 is_arm64_linux=$([ "$(uname)" = "Linux" ] && { [ "$(uname -m)" = "arm64" ] || [ "$(uname -m)" = "aarch64" ]; } && echo true || echo false)
 
-if [ ! command -v brew ] &>/dev/null; then
+if ! command -v brew &>/dev/null; then
   if [ $is_arm64_linux = true ]; then
     echo 'unable to install brew on ARM Linux'
   else
     echo 'installing brew'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    if [ command -v /usr/local/bin/brew ] &>/dev/null; then
+    if command -v /usr/local/bin/brew &>/dev/null; then
       echo 'setting path for macos brew'
       echo 'eval "$(/usr/local/bin/brew shellenv)"' >>~/.zshrc
       eval "$(/usr/local/bin/brew shellenv)"
     fi
 
-    if [ command -v /home/linuxbrew/.linuxbrew/bin/brew ] &>/dev/null; then
+    if command -v /home/linuxbrew/.linuxbrew/bin/brew &>/dev/null; then
       echo 'setting path for linux brew'
       echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
       eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -31,7 +31,7 @@ if ! command -v chezmoi &>/dev/null; then
   echo 'installing chezmoi'
 
   if [ $is_arm64_linux = true ]; then
-    sh -c "$(curl -fsLS get.chezmoi.io)"
+    sudo snap install chezmoi --classic
   else
     brew install chezmoi
   fi
@@ -39,7 +39,7 @@ else
   echo 'chezmoi is already installed'
 fi
 
-if [ ! test -d $(chezmoi source-path) ]; then
+if ! test -d $(chezmoi source-path); then
   echo 'initialising chezmoi'
   chezmoi init dahmon --ssh
 else
