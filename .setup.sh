b69ae2a58ae0ccfb10873acde34f4fc167058f90
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-02-16 12:11:00 +1100

    fix: Install chezmoi via curl for arm linux

--- .setup.sh
+++ .setup.sh
@@ -2,20 +2,26 @@
 
 set -e
 
-if ! command -v brew &>/dev/null; then
-  echo 'installing brew'
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+is_arm64_linux=$([ "$(uname)" = "Linux" ] && { [ "$(uname -m)" = "arm64" ] || [ "$(uname -m)" = "aarch64" ]; } && echo true || echo false)
 
-  if command -v /usr/local/bin/brew &>/dev/null; then
-    echo 'setting path for macos brew'
-    echo 'eval "$(/usr/local/bin/brew shellenv)"' >>~/.zshrc
-    eval "$(/usr/local/bin/brew shellenv)"
-  fi
+elif ! command -v brew &>/dev/null; then
+  if is_arm64_linux; then
+    echo 'unable to install brew on ARM Linux'
+  else
+    echo 'installing brew'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  if command -v /home/linuxbrew/.linuxbrew/bin/brew &>/dev/null; then
-    echo 'setting path for linux brew'
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    if command -v /usr/local/bin/brew &>/dev/null; then
+      echo 'setting path for macos brew'
+      echo 'eval "$(/usr/local/bin/brew shellenv)"' >>~/.zshrc
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
+
+    if command -v /home/linuxbrew/.linuxbrew/bin/brew &>/dev/null; then
+      echo 'setting path for linux brew'
+      echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
   fi
 else
   echo 'brew is already installed'
@@ -23,7 +29,12 @@ fi
 
 if ! command -v chezmoi &>/dev/null; then
   echo 'installing chezmoi'
-  brew install chezmoi
+
+  if is_arm64_linux; then
+    sh -c "$(curl -fsLS get.chezmoi.io)"
+  else
+    brew install chezmoi
+  fi
 else
   echo 'chezmoi is already installed'
 fi
