e65cff7293e20ec04b70fc53e76e1c9878d56492
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-06-11 13:53:35 +1000

    fix: Update Avante configuration

--- private_dot_config/nvim/lua/plugins/avante.lua
+++ private_dot_config/nvim/lua/plugins/avante.lua
@@ -3,19 +3,20 @@ return {
   event = "VeryLazy",
   opts = {
     provider = "openai",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o",
-      timeout = 30000,
-      temperature = 0,
-      max_completion_tokens = 8192,
-      reasoning_effort = "medium",
+    providers = {
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o",
+        timeout = 30000,
+        extra_request_body = {
+          temperature = 0,
+          max_completion_tokens = 8192,
+          reasoning_effort = "medium",
+        },
+      },
+      ollama = {
+        model = "qwen2.5-coder:7b",
+      },
     },
-    --provider = "ollama",
-    --ollama = {
-    --  model = "qwen2.5-coder:7b",
-    --  --model = "deepseek-coder-v2:16b",
-    --  stream = true,
-    --},
   },
 }
