3c1faf5e75f7879b150214caaa92dfb8091e8f5f
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-07-23 21:37:08 +1000

    feat: Add <leader>t keybind for ObsidianTemplate

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ObsidianNoteEnter",
+  callback = function(ev)
+    vim.keymap.set("n", "<leader>t", "<cmd>ObsidianTemplate<cr>", {
+      buffer = ev.buf,
+      desc = "Obsidian Template",
+    })
+  end,
+})
